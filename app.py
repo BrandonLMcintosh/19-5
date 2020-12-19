af478c17909da8736d9644a5a242ee33262e2676
Brandon McIntosh <Brandon.L.Mcintosh@gmail.com>
2020-12-19 12:23:28 -0800

    created necessary JS/HTML files. Started on routes

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 from boggle import Boggle
+from Flask import flask
+from flask_debugtoolbar import DebugToolbarExtension
 
 boggle_game = Boggle()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "ABCDEFG"
+debug = DebugToolbarExtension(app)
+
+@app.route("/")
+def index():
+    session["board"] = boggle_game.make_board()
+    return render_template("base.html")
+
